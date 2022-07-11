ec98d2b5f05d01f2e55c7c46092eb2dca55ef41e
Andreas Bauer <dev@andreasbauer.org>
2022-07-11 15:08:44 +0200

    Add args to main for token and repo

--- ocam/__main__.py
+++ ocam/__main__.py
@@ -1,17 +1,32 @@
 import requests
 import core
+import argparse
+
+def read_token_from_file(filename: str) -> str:
+    try:
+        access_token = open(filename,'r').readline().rstrip()
+        return access_token
+    except:
+        print('Unable to read "', filename, '" file')
+        return ''
 
 if __name__ == '__main__':
-    print("OCAM", core.get_version())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--token', help='GitHub token to access GitHub API')
+    parser.add_argument('repo', help='Repository that shall be analyzed')
+    args = parser.parse_args()
 
-    try:
-        access_token = open('token','r').readline().rstrip()
-    except:
-        print('Unable to read "./token" file')
+    access_token = args.token
+    if not access_token:
+        access_token = read_token_from_file('token')
+    if not access_token:
+        print('Unable to continue. No GitHub access token was provided!')
         quit()
+
+    print("OCAM", core.get_version())
+    repo = args.repo
     headers = {'Authorization': "Token " + access_token}
 
-    repo = 'PerssonAlbin/file-editor_OWN_CPP'
     url = f'https://api.github.com/repos/{repo}/pulls?state=all'
     total_pr_json = requests.get(url, headers=headers).json()
     print('All PRs for repository', repo, ':')
